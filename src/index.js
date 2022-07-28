403dbf6ca28ded8f9f444d203893ed18cf5e5539
lokki931 <taurusr2d2@gmail.com>
2022-07-28 16:34:39 +0300

    rewrite code add store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import state from "./redux/state";
-import { rerenderEntierTreee } from './render';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import store from "./redux/state";
 
-rerenderEntierTreee(state);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const rerenderEntierTreee = (state) => {
+
+    root.render(
+        <React.StrictMode>
+            <App
+                state={state}
+                // store={store}
+                addPost={store.addPost.bind(store)}
+                updateNewPostText={store.updateNewPostText.bind(store)}
+                addMessage={store.addMessage.bind(store)}
+                updateNewMessageText={store.updateNewMessageText.bind(store)}
+            />
+        </React.StrictMode>
+    );
+}
+
+rerenderEntierTreee(store.getState());
+store.subscribe(rerenderEntierTreee);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,119 +1,126 @@
-import { rerenderEntierTreee } from './../render';
-let state = {
+let store = {
+    _state: {
 
-    profilePage: {
-        posts: [
-            {
-                id: 1,
-                like: 7,
-                title: 'Hi'
-            },
-            {
-                id: 2,
-                like: 1,
-                title: 'How are you?'
-            },
-            {
-                id: 3,
-                like: 4,
-                title: 'Hi Nick'
-            },
-            {
-                id: 4,
-                like: 3,
-                title: 'Oh'
-            },
-            {
-                id: 5,
-                like: 2,
-                title: 'Haha'
-            }
-        ],
-        onPostValue: 'Add post name...'
+        profilePage: {
+            posts: [
+                {
+                    id: 1,
+                    like: 7,
+                    title: 'Hi'
+                },
+                {
+                    id: 2,
+                    like: 1,
+                    title: 'How are you?'
+                },
+                {
+                    id: 3,
+                    like: 4,
+                    title: 'Hi Nick'
+                },
+                {
+                    id: 4,
+                    like: 3,
+                    title: 'Oh'
+                },
+                {
+                    id: 5,
+                    like: 2,
+                    title: 'Haha'
+                }
+            ],
+            onPostValue: 'Add post name...'
+        },
+        dialogsPage: {
+            dialogs: [
+                {
+                    id: 1,
+                    name: 'Taras'
+                },
+                {
+                    id: 2,
+                    name: 'Dima'
+                },
+                {
+                    id: 3,
+                    name: 'Olga'
+                },
+                {
+                    id: 4,
+                    name: 'Yulia'
+                },
+                {
+                    id: 5,
+                    name: 'Anton'
+                }
+            ],
+            messages: [
+                {
+                    id: 1,
+                    message: 'Hi'
+                },
+                {
+                    id: 2,
+                    message: 'How are you?'
+                },
+                {
+                    id: 3,
+                    message: 'Hi Nick'
+                },
+                {
+                    id: 4,
+                    message: 'Oh'
+                },
+                {
+                    id: 5,
+                    message: 'Haha'
+                }
+            ],
+            onMessageValue: 'Add message..'
+        }
+
     },
-    dialogsPage: {
-        dialogs: [
-            {
-                id: 1,
-                name: 'Taras'
-            },
-            {
-                id: 2,
-                name: 'Dima'
-            },
-            {
-                id: 3,
-                name: 'Olga'
-            },
-            {
-                id: 4,
-                name: 'Yulia'
-            },
-            {
-                id: 5,
-                name: 'Anton'
-            }
-        ],
-        messages: [
-            {
-                id: 1,
-                message: 'Hi'
-            },
-            {
-                id: 2,
-                message: 'How are you?'
-            },
-            {
-                id: 3,
-                message: 'Hi Nick'
-            },
-            {
-                id: 4,
-                message: 'Oh'
-            },
-            {
-                id: 5,
-                message: 'Haha'
-            }
-        ],
-        onMessageValue: 'Add message..'
+    _callSubscribe() {
+        console.log('state rerenderEntierTreee');
+    },
+    getState() {
+        return this._state;
+    },
+    addPost() {
+        let newPost = {
+            id: 6,
+            like: 0,
+            title: this._state.profilePage.onPostValue
+        };
+
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.onPostValue = '';
+        this._callSubscribe(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.onPostValue = newText;
+        this._callSubscribe(this._state);
+    },
+    addMessage() {
+        let newMessage = {
+            id: 6,
+            message: this._state.dialogsPage.onMessageValue
+        };
+
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.onMessageValue = '';
+        this._callSubscribe(this._state);
+    },
+    updateNewMessageText(newText) {
+        this._state.dialogsPage.onMessageValue = newText;
+        this._callSubscribe(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscribe = observer;// observer pattern
     }
-
 };
 
-window.state = state;
+export default store;
 
-export let addPost = () => {
-    let newPost = {
-        id: 6,
-        like: 0,
-        title: state.profilePage.onPostValue
-    };
 
-    state.profilePage.posts.push(newPost);
-    state.profilePage.onPostValue = '';
-    rerenderEntierTreee(state);
-};
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.onPostValue = newText;
-    rerenderEntierTreee(state);
-};
-
-export let addMessage = () => {
-    let newMessage = {
-        id: 6,
-        message: state.dialogsPage.onMessageValue
-    };
-
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.onMessageValue = '';
-    rerenderEntierTreee(state);
-};
-
-export let updateNewMessageText = (newText) => {
-    state.dialogsPage.onMessageValue = newText;
-    rerenderEntierTreee(state);
-};
-
-export default state;
+window.store = store;
