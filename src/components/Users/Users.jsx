f7f7f4c16321e78b52d884b636cc3599014354c9
lokki931 <taurusr2d2@gmail.com>
2022-08-27 15:06:01 +0300

    get users from api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,26 @@
+import axios from 'axios';
 import React from 'react';
+import photoAva from '../../assets/images/ava.png';
 
 const Users = (props) => {
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items)
+                });
+        }
+    }
     return (
         <>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(user => <div key={user.id} style={{ display: 'flex' }}>
                     <span>
                         <div>
-                            <img src={user.photoUrl} alt={user.fullName} width='50px' />
+                            <img src={user.photos.small !== null ?
+                                user.photos.small :
+                                photoAva} alt={user.name} width='100px' />
                         </div>
                         <div>
                             {user.followed ?
@@ -19,12 +32,12 @@ const Users = (props) => {
                     </span>
                     <span style={{ display: 'flex' }}>
                         <span>
-                            <div>{user.fullName}</div>
+                            <div>{user.name}</div>
                             <div>{user.status}</div>
                         </span>
                         <span>
-                            <div>{user.location.country}</div>
-                            <div>{user.location.city}</div>
+                            {/* <div>{user.location.country}</div>
+                            <div>{user.location.city}</div> */}
                         </span>
                     </span>
                 </div>)
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,50 +4,50 @@ const SET_USERS = 'SET-USERS';
 
 let initialState = {
     users: [
-        {
-            id: 1,
-            fullName: 'Taras',
-            photoUrl: 'https://static1.funidelia.com/5150-f6_big2/seksualnij-kostum-dla-doroslih-nejtiri-z-avatara.jpg',
-            followed: true,
-            status: 'Hi i am programer js',
-            location: {
-                city: 'Khmel',
-                country: 'Ukraine'
-            }
-        },
-        {
-            id: 2,
-            fullName: 'Sasha',
-            photoUrl: 'https://static1.funidelia.com/5150-f6_big2/seksualnij-kostum-dla-doroslih-nejtiri-z-avatara.jpg',
-            followed: false,
-            status: 'Hi i am bild',
-            location: {
-                city: 'Kiev',
-                country: 'Ukraine'
-            }
-        },
-        {
-            id: 3,
-            fullName: 'Dima',
-            photoUrl: 'https://static1.funidelia.com/5150-f6_big2/seksualnij-kostum-dla-doroslih-nejtiri-z-avatara.jpg',
-            followed: true,
-            status: 'Hi i am programer php',
-            location: {
-                city: 'Volochusk',
-                country: 'Ukraine'
-            }
-        },
-        {
-            id: 4,
-            fullName: 'Olya',
-            photoUrl: 'https://static1.funidelia.com/5150-f6_big2/seksualnij-kostum-dla-doroslih-nejtiri-z-avatara.jpg',
-            followed: false,
-            status: 'Hi i am teacher',
-            location: {
-                city: 'Volochusk',
-                country: 'Ukraine'
-            }
-        }
+        // {
+        //     id: 1,
+        //     fullName: 'Taras',
+        //     photoUrl: 'https://static1.funidelia.com/5150-f6_big2/seksualnij-kostum-dla-doroslih-nejtiri-z-avatara.jpg',
+        //     followed: true,
+        //     status: 'Hi i am programer js',
+        //     location: {
+        //         city: 'Khmel',
+        //         country: 'Ukraine'
+        //     }
+        // },
+        // {
+        //     id: 2,
+        //     fullName: 'Sasha',
+        //     photoUrl: 'https://static1.funidelia.com/5150-f6_big2/seksualnij-kostum-dla-doroslih-nejtiri-z-avatara.jpg',
+        //     followed: false,
+        //     status: 'Hi i am bild',
+        //     location: {
+        //         city: 'Kiev',
+        //         country: 'Ukraine'
+        //     }
+        // },
+        // {
+        //     id: 3,
+        //     fullName: 'Dima',
+        //     photoUrl: 'https://static1.funidelia.com/5150-f6_big2/seksualnij-kostum-dla-doroslih-nejtiri-z-avatara.jpg',
+        //     followed: true,
+        //     status: 'Hi i am programer php',
+        //     location: {
+        //         city: 'Volochusk',
+        //         country: 'Ukraine'
+        //     }
+        // },
+        // {
+        //     id: 4,
+        //     fullName: 'Olya',
+        //     photoUrl: 'https://static1.funidelia.com/5150-f6_big2/seksualnij-kostum-dla-doroslih-nejtiri-z-avatara.jpg',
+        //     followed: false,
+        //     status: 'Hi i am teacher',
+        //     location: {
+        //         city: 'Volochusk',
+        //         country: 'Ukraine'
+        //     }
+        // }
     ]
 };
 
