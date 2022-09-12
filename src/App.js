f00ca2eba14594f832d852023f0efe1bf2bc50f4
lokki931 <taurusr2d2@gmail.com>
2022-09-12 17:32:19 +0300

    fix render profile

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import "./App.css";
-import React from "react";
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import { Routes, Route } from "react-router-dom";
+import './App.css';
+import React from 'react';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
+import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
 
 const App = (props) => {
-    return (
-        <div className="wrapper">
-            <Header />
-            <Navbar />
-            <div className="content">
-                <Routes>
-                    <Route path="/profile" element={<ProfileContainer />}>
-                        <Route path=":userId" element={<ProfileContainer />} />
-                    </Route>
-                    <Route path='/dialogs/*' element={
-                        <DialogsContainer />
-                    } />
-                    <Route path='/news' element={<News />} />
-                    <Route path='/music' element={<Music />} />
-                    <Route path='/settings' element={<Settings />} />
-                    <Route path='/users' element={<UsersContainer />} />
-                </Routes>
-            </div>
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <Header />
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/profile" element={<ProfileContainer />}>
+            <Route path=":userId" element={<ProfileContainer />} />
+          </Route>
+          <Route path="/dialogs/*" element={<DialogsContainer />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/users" element={<UsersContainer />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import Profile from './Profile';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 // import s from './Profile.module.css';
 import { connect } from 'react-redux';
 import { setProfile } from './../../redux/profile-reducer';
 
-
 class ProfileContainer extends React.Component {
-
-    componentDidMount() {
-        let userId = this.props.match.params.userId;
-        if (!userId) userId = 2;
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
-            .then(response => {
-                this.props.setProfile(response.data);
-            });
-
-    }
-    render() {
-        return <Profile {...this.props} profile={this.props.profile} />;
-    }
-
+  componentDidMount() {
+    let userId = this.props.router.params.userId || 2;
+    // if (!userId) userId = 2;
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then((response) => {
+      this.props.setProfile(response.data);
+    });
+  }
+  componentDidUpdate() {
+    this.componentDidMount();
+  }
+  render() {
+    return <Profile {...this.props} profile={this.props.profile} />;
+  }
 }
-const mapStateToProps = state => {
-    return {
-        profile: state.profilePage.profile,
-    }
-}
-export function withRouter(Children) {
-    return (props) => {
+const mapStateToProps = (state) => {
+  return {
+    profile: state.profilePage.profile,
+  };
+};
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return <Component {...props} router={{ location, navigate, params }} />;
+  }
 
-        const match = { params: useParams() };
-        return <Children {...props} match={match} />
-    }
+  return ComponentWithRouterProp;
 }
 
 const ComponentWhithRouter = withRouter(ProfileContainer);
 
 export default connect(mapStateToProps, {
-    setProfile
+  setProfile,
 })(ComponentWhithRouter);
